6beaf51d93937d0e65bc5aea1721d6764ef704f1
Tomas Figueroa Estrada <tfiguero@car14s1.42barcelona.com>
2023-09-07 22:01:08 +0200

    optimizacion de chunker en proceso

--- chunker.c
+++ chunker.c
@@ -6,7 +6,7 @@
 /*   By: tfiguero <tfiguero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 13:59:59 by tfiguero          #+#    #+#             */
-/*   Updated: 2023/09/06 15:32:44 by tfiguero         ###   ########.fr       */
+/*   Updated: 2023/09/07 22:00:26 by tfiguero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	ft_chunko(t_struct *aha, int chunk_c)
 
 	limit_chunk = aha->len / chunk_c;
 	j = limit_chunk;
+	limit_chunk += aha->len % chunk_c;
 	while (chunk_c > 0)
 	{
 		while (i < limit_chunk && aha->lena > 0)
@@ -39,16 +40,30 @@ void	ft_chunko(t_struct *aha, int chunk_c)
 	}
 }
 
-int	ft_check_ordered(t_struct *aha)
+void	optimizacon_chunkero(t_struct *aha, int li_chunk)
 {
-	int	i;
-
-	i = 0;
-	while (i < aha->lena - 1)
+	int j;
+	int k;
+	
+	j = 0;
+	while (j < aha->len)
 	{
-		if (aha->stacka[i] > aha->stacka[i + 1])
-			return (1);
-		i++;
+		if (aha->stacka[j] <= li_chunk)
+			k = j;
+		j++;
 	}
-	return (0);
+	while (i > 0)
+	{
+		if (aha->stacka[i] <= li_chunk)
+		{
+		}
+	}
+	//Comparar i con len-J para ver cual esta mas cerca de la posicion 0 y entonces decidir si hago rra o ra
+	//En el caso de que esten a la misma distancia mirar cual esta mas cerca del stackb[0]
+	if(k <= aha->lena / 2)
+		while(aha->stacka[0] != i)
+			ra(aha);
+	else
+		while(aha->stacka[0] != i)
+			rra(aha);
 }
